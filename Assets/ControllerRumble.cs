ce30e125eb91a1658d8e93434b9268eed0ae24e4
FADCDD\jj.rosenberg <jj.rosenberg@metachef.nl>
2023-05-26 16:40:34 +0200

    Reset Button

---
+++ Assets/ControllerRumble.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Interaction;
+using UnityEngine.XR;
+using UnityEngine.XR.OpenXR.Input;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ControllerRumble : MonoBehaviour
+{
+    [SerializeField] XRBaseController rightHand;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        rightHand.SendHapticImpulse(0.7f, 2f);
+    }
+}
